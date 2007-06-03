1be56c775fe12dcc91f07e1c410a34a8b4f45abc
Yevgen Muntyan <muntyan@tamu.edu>
2007-06-02 20:33:51 -0500

    New scripts

--- scripts/ggap.sh
+++ scripts/ggap.sh
@@ -13,6 +13,8 @@ export MOO_DATA_DIRS=$ggap_dir/share/ggap
 export MOO_LIB_DIRS=$ggap_dir/lib/ggap
 export MOO_LOCALE_DIR=$ggap_dir/share/locale
 
+export PATH=$ggap_dir/bin:$PATH
+
 export LD_LIBRARY_PATH=$ggap_dir/lib:$LD_LIBRARY_PATH
 
 exec $ggap_dir/bin/ggap $*
