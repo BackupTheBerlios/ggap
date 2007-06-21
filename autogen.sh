d208d539ab596a9916a70299247df83d99499d9a
Yevgen Muntyan <muntyan@tamu.edu>
2007-06-20 21:37:08 -0600

    Require automake-1.10

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#! /bin/sh
 
 builddir=`pwd`
 
@@ -17,9 +17,9 @@ fi
 cd $srcdir
 echo "Generating configuration files..."
 
-ACLOCAL=${ACLOCAL:-aclocal-1.9}
-ACLOCAL_FLAGS="$ACLOCAL_FLAGS $aclocal_extra"
-AUTOMAKE=${AUTOMAKE:-automake-1.9}
+ACLOCAL=${ACLOCAL:-aclocal-1.10}
+ACLOCAL_FLAGS="$aclocal_extra $ACLOCAL_FLAGS"
+AUTOMAKE=${AUTOMAKE:-automake-1.10}
 
 LIBTOOLIZE=${LIBTOOLIZE:-libtoolize}
 
@@ -30,9 +30,14 @@ echo $LIBTOOLIZE --automake --copy --force
 $LIBTOOLIZE --automake --copy --force || exit $?
 
 echo glib-gettextize --force --copy
-glib-gettextize --force --copy
+glib-gettextize --force --copy || exit $?
 echo intltoolize --automake --force --copy
-intltoolize --automake --force --copy
+intltoolize --automake --force --copy || exit $?
+
+if [ -d po-gsv ] ; then
+  echo sed 's/@GETTEXT_PACKAGE@/@GETTEXT_PACKAGE@-gsv/' po/Makefile.in.in '>' po-gsv/Makefile.in.in
+  sed 's/@GETTEXT_PACKAGE@/@GETTEXT_PACKAGE@-gsv/' po/Makefile.in.in > po-gsv/Makefile.in.in || exit $?
+fi
 
 echo $ACLOCAL $ACLOCAL_FLAGS
 $ACLOCAL $ACLOCAL_FLAGS || exit $?
