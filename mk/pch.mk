da441cc4e8819024c36d60ec7c0993474d7ddc00
Yevgen Muntyan <muntyan@tamu.edu>
2008-08-09 21:37:50 -0500

    Made ALL_CXXFLAGS not needed

--- mk/pch.mk
+++ mk/pch.mk
@@ -1,22 +1,30 @@
 # -*- makefile -*-
 
-pch_name = precompiled-header-name
-pch_file = $(pch_name).gch
+# $(PCH_HEADER) must be the header file to precompile
 
-EXTRA_DIST += $(pch_header)
+EXTRA_DIST += $(PCH_HEADER)
+
+BASE_COMPILE = $(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(AM_CPPFLAGS) $(CPPFLAGS) $(AM_CFLAGS) $(CFLAGS)
+BASE_CXXCOMPILE = $(CXX) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(AM_CPPFLAGS) $(CPPFLAGS) $(ALL_CXXFLAGS) $(CXXFLAGS)
+
+COMPILE = $(BASE_COMPILE)
+CXXCOMPILE = $(BASE_CXXCOMPILE)
 
 if MOO_ENABLE_PCH
 
-BUILT_SOURCES += $(pch_file)
-DISTCLEANFILES += $(pch_file)
+BUILT_SOURCES += $(pch_files)
+CLEANFILES += $(pch_files)
 
-AM_CXXFLAGS = $(ALL_CXXFLAGS) -include $(pch_name) -Winvalid-pch
+pch_name = precompiled-header-name
+pch_dir = $(pch_name).gch
+pch_files = $(pch_dir)/c $(pch_dir)/c++
 
-$(pch_name).gch: $(pch_header)
-	$(CXX) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(AM_CPPFLAGS) $(CPPFLAGS) $(ALL_CXXFLAGS) $(CXXFLAGS) $< -o $@
+$(pch_dir)/c: $(PCH_HEADER)
+	mkdir -p $(pch_dir) && $(BASE_COMPILE) -x c-header -c $< -o $@
+$(pch_dir)/c++: $(PCH_HEADER)
+	mkdir -p $(pch_dir) && $(BASE_CXXCOMPILE) -x c++-header -c $< -o $@
 
-else
-
-AM_CXXFLAGS = $(ALL_CXXFLAGS)
+COMPILE += -include $(pch_name) -Winvalid-pch
+CXXCOMPILE += -include $(pch_name) -Winvalid-pch
 
 endif
