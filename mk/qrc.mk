d0f421d7e9797bd86be5d0d49f6798526f56d396
Yevgen Muntyan <muntyan@tamu.edu>
2008-08-13 12:55:12 -0500

    Use @MODULE@ so automake can understand it

--- mk/qrc.mk
+++ mk/qrc.mk
@@ -5,11 +5,10 @@
 # qrc
 #
 # input: QT_QRC_FILES - list of .qrc files
-#        QT_QRC_NAME
 #        QT_QRC_DEPS
 #
 
-ugly_qrc_cpp = $(QT_QRC_NAME)-qrc.cpp
+ugly_qrc_cpp = @MODULE@-qrc.cpp
 $(ugly_qrc_cpp): $(QT_QRC_FILES) $(QT_QRC_DEPS)
 	$(QT_RCC) -o $(ugly_qrc_cpp).tmp -name $(QT_QRC_NAME) $(addprefix $(srcdir)/,$(QT_QRC_FILES)) && mv $(ugly_qrc_cpp).tmp $(ugly_qrc_cpp)
 
